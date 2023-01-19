180339bc985edb0f59c6e2f2d5aa8d8be94f4830
Dmitrij <dimaparfenyk1990@gmail.com>
2023-01-19 13:04:13 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -457,103 +457,305 @@
 // multiply(1, 2);
 // multiply(1, 2, 3);
 // multiply(1, 2, 3, 4);
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
 
-function getAllPropValues(propName) {
-  // Change code below this line
-  let values = [];
-  for (const product of products) {
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let values = [];
+//   for (const product of products) {
   
-    if (product.hasOwnProperty(propName)) {
-      values.push(product[propName])
-    }
-    }
-    return values
-    // Change code above this line
-  }
+//     if (product.hasOwnProperty(propName)) {
+//       values.push(product[propName])
+//     }
+//     }
+//     return values
+//     // Change code above this line
+//   }
 
-console.log(getAllPropValues("fgjj"))
+// console.log(getAllPropValues("fgjj"))
 
-function makeTask(data) {
-  const completed = false;
-  const category = "General";
-  const priority = "Normal";
-  // Change code below this line
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
+//   // Change code below this line
 
-  // Change code above this line
+//   // Change code above this line
+// }
+
+// function findMatches(arr,...args) {
+//   const matches = []; // Don't change this line
+// for (const arg of args) {
+//   if (arr.includes(arg)) {
+//    matches.push(arg)
+//   };
+//   };
+//   // Change code above this line
+//   return matches;
+// };
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Stone skin", price: 520 },
+//     { name: "Dragon breath", price: 780 },
+//   ],
+//   // Change code below this line
+//   // getPotions() {
+//   //   return this.potions;
+//   // },
+//   // addPotion(newPotion) {
+//   //   for (let i = 0; i < this.potions.length; i+=1) {
+//   //     if (this.potions[i].name===newPotion.name) {
+//   //       return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//   //     }
+//   //     this.potions.push(newPotion);
+//   //   }
+//   // },
+//   // removePotion(potionName) {
+//   //   for (let i = 0; i < this.potions.length; i+=1) {
+//   //     if (this.potions[i].name === potionName) {
+//   //       this.potions.splice(i,1)
+//   //     }
+//   //   }
+//   //    return `Potion ${potionName} is not in inventory!`;
+//   // },
+//   updatePotionName(oldName, newName) {
+//     // const potionIndex = this.potions.indexOf(oldName);
+
+//     // if (potionIndex === -1) {
+//     //   return `Potion ${oldName} is not in inventory!`;
+//     // }
+
+//     // this.potions.splice(potionIndex, 1, newName);
+//     for (const potion of this.potions) {
+//       if (potion.name === oldName) {
+//         potion.name = newName;
+//       }
+//     }
+//     return `Potion ${oldName} is not in inventory!`;
+//   },
+//   // Change code above this line
+// };
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth")
+// console.log(atTheOldToad.potions);
+
+
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => console.log(`Индекс ${index}, значение ${number}`);
+
+// numbers.forEach(logMessage);
+
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+ 
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`Соединяем с ${name}, ожидайте...`);
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+// }
+// processCall("Poly", takeCall, leaveHoloMessage);
+// processCall("Mango", takeCall, activateAnsweringMachine);
+
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, onSuccess, onError) {
+//     const { pizzas } = this;
+//      for (const pizza of pizzas) {
+//       if (pizza === pizzaName) {
+//         console.log(pizzaName);
+//       }
+//     }
+//    }
+//   };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+// pizzaPalace.order('Four meats', makePizza, onOrderError);
+// pizzaPalace.order('Big Mike', makePizza, onOrderError);
+// console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+
+// // const isPizzaAvailable = this.pizzas.includes(pizzaName);
+// //     if (isPizzaAvailable) {
+// //       return onSuccess(pizzaName)
+// //     }
+// //    return onError('Такой пицы нет')
+
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+// firstArray.forEach(number=>{
+//   if(secondArray.includes(number)){
+//     commonElements.push(number)
+//   }
+// })
+  
+//   return commonElements;
+//   // Change code above this line
+// }
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   let newArr=[];
+//   numbers.forEach(number=>{
+//     if (number % 2 === 0) {
+//     return  newArr.push(number+value);
+//     }
+//     newArr.push(number)
+   
+//   })
+//   console.log(newArr)
+//  return newArr
+//   // Change code above this line
+// }
+function changeEven(numbers, value) {
+  const newArr = numbers.map(number => {
+    const isNumberEven = number % 2 === 0;
+   return isNumberEven?(number + value):number
+  //   if (isNumberEven) {
+  //   return  number + value;
+  //   }
+  //  return number
+  })
+  console.log(newArr)
+  return newArr
 }
+changeEven([1, 2, 3, 4, 5], 10)
 
-function findMatches(arr,...args) {
-  const matches = []; // Don't change this line
-for (const arg of args) {
-  if (arr.includes(arg)) {
-   matches.push(arg)
-  };
-  };
-  // Change code above this line
-  return matches;
+const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// Change code below this line
+const planetsLengths = planets.map(planet => planet.length );
+console.log(planetsLengths)
+
+
+const friends = [
+  "Briana Decker", "Sharron Pace", "Sharron Pace", "Marilyn Mcintosh",
+  "Padilla Garrison", "Naomi Buckner", "Goldie Gentry", "Aisha Tran",
+  "Jordan Sampson", "Eddie Strong", "Jacklyn Lucas", "Linda Chapman",
+  "Goldie Gentry", "Briana Decker"]
+
+const getFriends = (users) => {
+   const allFriends=users.flatMap(user=>user.friends);
+  return allFriends.filter((friend,index,arr)=>arr.indexOf(friend)===index)
 };
 
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Stone skin", price: 520 },
-    { name: "Dragon breath", price: 780 },
-  ],
-  // Change code below this line
-  // getPotions() {
-  //   return this.potions;
-  // },
-  // addPotion(newPotion) {
-  //   for (let i = 0; i < this.potions.length; i+=1) {
-  //     if (this.potions[i].name===newPotion.name) {
-  //       return `Error! Potion ${newPotion.name} is already in your inventory!`;
-  //     }
-  //     this.potions.push(newPotion);
-  //   }
-  // },
-  // removePotion(potionName) {
-  //   for (let i = 0; i < this.potions.length; i+=1) {
-  //     if (this.potions[i].name === potionName) {
-  //       this.potions.splice(i,1)
-  //     }
-  //   }
-  //    return `Potion ${potionName} is not in inventory!`;
-  // },
-  updatePotionName(oldName, newName) {
-    // const potionIndex = this.potions.indexOf(oldName);
+const sortByName = users => [...users].sort((firstUser, secondUser) => firstUser.name.localeCompare(secondUser.name));
 
-    // if (potionIndex === -1) {
-    //   return `Potion ${oldName} is not in inventory!`;
-    // }
-
-    // this.potions.splice(potionIndex, 1, newName);
-    for (const potion of this.potions) {
-      if (potion.name === oldName) {
-        potion.name = newName;
-      }
-    }
-    return `Potion ${oldName} is not in inventory!`;
+const users=[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
   },
-  // Change code above this line
-};
-atTheOldToad.updatePotionName("Dragon breath", "Polymorth")
-console.log(atTheOldToad.potions);
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
 
+// const getNamesSortedByFriendCount =
+//   users => [...users].sort((firstUser, secondUser) => firstUser.friends.length - secondUser.friends.length)
+//     .flatMap(({ friends }) => friends)
+//     // .filter((user,index, arr)=>arr.indexOf(user)===index);
+  
+// console.log(getNamesSortedByFriendCount(users)) 
 
+// const getSortedFriends = users => 
+//   [...users].flatMap(({ friends }) => friends)
+//     .filter((friend, index, arr) => arr.indexOf(friend) === index)
+//   .sort((firstFriend,secondFriend)=>firstFriend.localeCompare(secondFriend));
+// console.log(getSortedFriends(users))
 
-const numbers = [5, 10, 15, 20, 25];
-
-const logMessage = (number, index) => console.log(`Индекс ${index}, значение ${number}`);
-
-numbers.forEach(logMessage);
-
-// Декларативный подход
-const numbersArr = [1, 2, 3, 4, 5];
-const filteredNumbers = numbersArr.filter(value => value > 3);
-console.log(filteredNumbers); // [4, 5]
+const getTotalBalanceByGender = (users, gender) => [...users].filter(user => user.gender === gender)
+  .reduce((total, user) => total+user.balance, 0);
+console.log(getTotalBalanceByGender(users, "male"));
+console.log(getTotalBalanceByGender(users, "female"));
